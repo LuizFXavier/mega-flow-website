2033bf17dc81601489183ee4bf59a59f75c0334f
LuizFXavier <zap422238@gmail.com>
2026-05-30 20:45:27 -0400

    feat: Configura o correto uso da API para realizar login

---
+++ src/services/authService.ts
@@ -0,0 +1,20 @@
+import type { LoginData } from "@/types/LoginData";
+import api from "./api"
+import { jwtDecode } from "jwt-decode";
+import type { UserAPI } from "@/types/User";
+
+export const authService = {
+  
+    // Função que recupera o bearer Token baseado na api
+    async login(userData:LoginData):Promise<string> {
+
+        try{
+            const {data} = await api.post('/auth/signin', userData);
+            
+            return data.tokenDeAcesso;
+        }
+        catch(e){
+            return ''
+        }
+    }
+}
