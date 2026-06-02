72a6ea0e586a74dbdfb419e762bf91d6503024e6
LuizFXavier <zap422238@gmail.com>
2026-06-01 22:28:34 -0400

    fix: Correção da cor principal de fundo

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <main class="bg-purple-800 h-screen">
+  <main class="bg-gray-border h-screen">
   <RouterView />
   </main>
 </template>
-
-<style scoped>
-/* main{ */
-  /* background-color: rebeccapurple; */
-/* } */
-</style>
