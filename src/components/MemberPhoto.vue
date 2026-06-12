7f14dede1de8d858430671f8f610ae3c8c14812d
LuizFXavier <zap422238@gmail.com>
2026-06-12 16:28:16 -0400

    feat: Listagem de fotos de membros no card de projetos

---
+++ src/components/MemberPhoto.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+interface Props{
+    photoUrl:string
+}
+
+const {photoUrl} = defineProps<Props>();
+
+</script>
+
+<template>
+    <img :src="photoUrl"
+         class="rounded-full
+                h-full
+                aspect-square"/>
+</template>
--- src/components/projectsPage/ProjectCard.vue
+++ src/components/projectsPage/ProjectCard.vue
@@ -1,17 +1,41 @@
 <script setup lang="ts">
 import type { MemberMap } from '@/types/Member';
 import type { Project } from '@/types/Project';
+import { computed } from 'vue';
+import MemberPhoto from '../MemberPhoto.vue';
+import { memberService } from '@/services/memberService';
+import { useMemberStore } from '@/stores/memberStore';
 
 interface Props{
     project:Project;
 }
 
+const visibleMembers = computed(()=>{
+    return project.membros.slice(0,3);
+})
+
+const extraMembers = computed(()=>{
+    return project.membros.length - visibleMembers.value.length;
+})
+
 const {project} = defineProps<Props>();
 
 const emit = defineEmits(['openModal']);
 
+const memberStore = useMemberStore();
+
 function handleClick(){
     emit('openModal');
+}
+
+function formatDate(date: string | null | undefined): string {
+    if (!date) return "";
+    const d = new Date(date);
+    return d.toLocaleDateString("pt-BR", {
+        day: "2-digit",
+        month: "short",
+        timeZone: "UTC",
+    });
 }
 
 </script>
@@ -29,6 +53,8 @@ function handleClick(){
                 hover:border
                 hover:border-[#7C7C7C]/40
                 cursor-pointer
+                
+                flex flex-col
                 ">
         <h1 class="text-xl">
             {{ project.titulo }}
@@ -37,7 +63,8 @@ function handleClick(){
         <div class="flex flex-row 
                     justify-between 
                     text-xs
-                    py-3">
+                    py-3
+                    h-6/10">
             <div class="flex place-items-center">
                 <img src="/icon/projects/building.png"/>
                 <h2>
@@ -46,28 +73,46 @@ function handleClick(){
                 </h2>
             </div>
 
-            <div class="flex place-items-center">
+            <div v-if="formatDate(project.dataConclusao)" 
+                class="flex place-items-center">
                 <img src="/icon/projects/calendar.png"/>
-                <h2>
-                    Prazo <br>
-                    <strong>12 jun</strong>
-                </h2>
+                <div class="flex flex-col place-items-start">
+                    <h2>
+                        Prazo
+                    </h2>
+                    <strong>{{ formatDate(project.dataConclusao) }}</strong>
+
+                </div>
             </div>
 
             <div class="flex place-items-center">
                 <img src="/icon/projects/wallet.png"/>
                 <h2>
                     Orçamento <br>
-                    <strong>{{ project.orcamento }}</strong>
+                    
+                    <strong> R$ {{ project.orcamento.toLocaleString("pt-BR", {
+                                minimumFractionDigits: 2,
+                                maximumFractionDigits: 2,
+                            }) }}</strong>
                 </h2>
             </div>
         </div>
 
-        <!-- <div v-for="{memberRga, funcao} in project.participants">
-            <h2>
-                funcao, {{ memberMap.get(memberRga) }}
-            </h2>
-        </div> -->
+        <div class="min-h-0 
+                    flex-1 flex flex-row">
+            <MemberPhoto v-for="member in visibleMembers"
+                         :photo-url="memberStore.getPhoto(member.rga)"
+                         class="bg-[#CCCCCC]"/>
+            <div v-if="extraMembers > 0"
+                 class="bg-[#CCCCCC]
+                        aspect-square
+                        rounded-full
+                        flex justify-center
+                        place-items-center
+                        text-[#560197]">
+                +{{ extraMembers }}
+            </div>
+        </div>
 
     </div>
 
