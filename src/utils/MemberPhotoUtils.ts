23433a9c804f6e99bee3e82e8cd5bdd65ff19487
cck <santosguilherme783@gmail.com>
2026-06-12 22:12:10 -0400

    feat: add util de comprimir fotos

---
+++ src/utils/MemberPhotoUtils.ts
@@ -0,0 +1,10 @@
+import imageCompression from "browser-image-compression";
+
+export async function compressImage(file: File): Promise<File> {
+  return await imageCompression(file, {
+    maxSizeMB: 0.5,
+    maxWidthOrHeight: 300,
+    useWebWorker: true,
+    fileType: "image/png",
+  });
+}
