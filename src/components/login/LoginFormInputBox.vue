ca940bbdf18399431262f8bf9fc24423df7098bd
LuizFXavier <zap422238@gmail.com>
2026-06-13 23:08:04 -0400

    feat: Implementa a mensagem de erro quando a senha está incorreta

--- src/components/login/LoginFormInputBox.vue
+++ src/components/login/LoginFormInputBox.vue
@@ -2,7 +2,6 @@
     <div class="text-[#939393] bg-[#2B2B2B] 
                   border border-[#474747] rounded-2xl 
                   h-2/10 w-full
-                  mb-8
                   flex
                   px-4
                   items-center
--- src/components/login/TheLoginForm.vue
+++ src/components/login/TheLoginForm.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 // TODO: verificar se e-mail e senha foram colocados
 
-import { reactive, ref } from 'vue'
+import { reactive, ref, watch } from 'vue'
 import LoginFormInputBox from './LoginFormInputBox.vue';
 import { useAuthStore } from '@/stores/authStore.ts';
 import { useRouter } from 'vue-router';
@@ -11,9 +11,15 @@ const authStore = useAuthStore();
 
 const showPassword = ref<Boolean>(false);
 
-const user = reactive<LoginData>({email:'Jubileu@megaflow.com', senha:'JubileuAoMolho'});
+const user = reactive<LoginData>({email:'', senha:''});
+
+watch(user, ()=>{
+    errorState.value = false;
+})
 
 const router = useRouter();
+
+const errorState = ref<boolean>(false);
 
 async function handleLogin(){
     try{
@@ -24,7 +30,7 @@ async function handleLogin(){
         router.push(to)
     }
     catch(e){
-
+        errorState.value = true;
     }
 }
 </script>
@@ -47,25 +53,33 @@ async function handleLogin(){
                         Acesse sua conta Mega
             </h2>
             
-            <form class="w-full h-4/10 flex flex-col justify-center">
-                <LoginFormInputBox>
+            <form class="w-full h-4/10 flex flex-col justify-center gap-4 relative">
+                <LoginFormInputBox :class=" errorState ? 'border-[#FF0000]' : ''">
                     
-                    <img src="/icon/mail.png" 
+                    <img src="/icon/mail.png"
                          class="w-6 h-5"/>
 
                     <input v-model="user.email"
                            type="e-mail" 
-                           class="h-full w-9/10"/>
+                           class="h-full w-9/10"
+                           placeholder="mega@email.com"/>
 
-                    <div class="w-5 h-5"></div>
+                    <div  class="w-5 h-5">
+                        <img v-if="errorState" 
+                                src="/icon/alert.png" 
+                                class="w-full h-full object-contain" 
+                                alt="Mostrar/Esconder senha"
+                            />
+                    </div>
                 </LoginFormInputBox>
                     
-                <LoginFormInputBox>
+                <LoginFormInputBox :class=" errorState ? 'border-[#FF0000]' : ''">
                     <img src="/icon/lock.png" class="w-5 h-6"/>
 
                     <input v-model="user.senha"
                            :type="showPassword ? 'text' : 'password'" 
-                           class="h-full w-9/10"/>
+                           class="h-full w-9/10"
+                           placeholder="Mega-senha"/>
 
                     <button type="button" 
                             @click="showPassword = !showPassword"
@@ -73,15 +87,25 @@ async function handleLogin(){
                             flex items-center justify-center 
                             focus:outline-none opacity-80 hover:opacity-100 transition-opacity"
                     >
-                        <img 
+                        <img v-if="!errorState" 
                             :src="showPassword ? '/icon/eye-open.png' : '/icon/eye-close.png'" 
+                            class="w-full h-full object-contain" 
+                            alt="Mostrar/Esconder senha"
+                        />
+                        <img v-else 
+                            :src="showPassword ? '/icon/eye-open.png' : '/icon/alert.png'" 
                             class="w-full h-full object-contain" 
                             alt="Mostrar/Esconder senha"
                         />
                     </button>
                 </LoginFormInputBox>
                 
+                <p  v-if="errorState"
+                    class="text-[#FF0000] text-sm absolute bottom-5">
+                    Seu e-mail ou senha estão incorretos.
+                </p>
             </form>
+            
             <button @click="handleLogin"
                     class="h-8/100 w-full
                          bg-[#8128F4] 
